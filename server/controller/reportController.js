920121fa1cd2dec69c06078fe8f559cfd7d073bf
Harshit22429 <singhtqp22429@gmail.com>
2022-12-11 16:26:07 +0530

    This is final and Accurate Report

--- server/controller/reportController.js
+++ server/controller/reportController.js
@@ -21,49 +21,41 @@ const getCompleteReport = async (req, res) => {
         console.log(slpList)
 
         const slpId = slpList.map((x) => x.userId)
-        console.log(slpId)
-        // const userId = 1031
-        // const desposition = await User.aggregate([
-        //     {
-        //         $lookup: {
-        //             from: "Desposition",
-        //             localField: "userId",
-        //             foreignField: "userId",
-        //             as: "despostion_details"
-        //         }
-        //     }
-        // ])
-        // console.log(desposition)
+        const slpName = slpList.map((x) => x.name)
 
-
-        // const desposition =  await Desposition.aggregate([
-        //     { $match: { userId: parentId } },
-        //     { $group: { _id: "$_id",desposition:{$first:"$desposition"}, count: { $sum: 1 } } }
-
-        // ])
-        // console.log(desposition)
         const despositionReport = []
         for (i = 0; i < slpId.length; i++) {
             slpCurrentId = slpId[i];
+            slpCurrentName = slpName[i];
+            // despositionReport.push({SPName:slpCurrentName})
             const detail = await Desposition.aggregate([
                 { $match: { userId: slpCurrentId } },
                 { $group: { _id: "$desposition", count: { $sum: 1 } } }
                 // {$project : {_id:0,_id:0,desposition:1, count:1}}
             ])
-            const singleDetail = Report_List2[detail[i]._id]
-            const singleDetailCount = detail[i].count
-            console.log(singleDetail, singleDetailCount)
+
+            let slpSeperateDetail = []
+            for (j = 0; j < detail.length; j++) {
+                singleDetail = Report_List2[detail[j]._id]
+                singleDetailCount = detail[j].count
+                // console.log(slpCurrentId)
+                // console.log(singleDetail, singleDetailCount)
+                slpSeperateDetail.push({
+                    despositionName: singleDetail,
+                    count: singleDetailCount
+                })
+            }            
+
             despositionReport.push({
-                despName: singleDetail,
-                count: singleDetailCount
+                slpName: slpCurrentName,
+                slpDetails: slpSeperateDetail
             })
-            // despositionReport.push(detail)
         }
 
 
 
-        console.log(despositionReport)
-        res.send(despositionReport)
+        // console.log(despositionReport)
+        res.json(despositionReport)
     } catch (error) {
         console.log(error)
     }
