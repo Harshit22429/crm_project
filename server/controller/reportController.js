01bdf49f0fa686c006d3440022ae836a57190e36
Harshit22429 <singhtqp22429@gmail.com>
2022-12-11 15:43:16 +0530

    testing Report api but this work only one slp report

--- server/controller/reportController.js
+++ server/controller/reportController.js
@@ -1,7 +1,7 @@
 const Desposition = require('../model/desposition')
 const Customer = require('../model/cutomer')
 const User = require('../model/user');
-const Repot_List = require("../config/Report_list")
+const Report_List2 = require("../config/Report_list2")
 const getCompleteReport = async (req, res) => {
     try {
 
@@ -15,16 +15,13 @@ const getCompleteReport = async (req, res) => {
                         { userId: parentId }
                     ]
                 },
-                
             },
-
             { $project: { _id: 0, name: 1, userId: 1 } },
-
         ])
         console.log(slpList)
 
-        const slpId = slpList.map((x)=>x.userId)
-        console.log(slpId) 
+        const slpId = slpList.map((x) => x.userId)
+        console.log(slpId)
         // const userId = 1031
         // const desposition = await User.aggregate([
         //     {
@@ -38,22 +35,29 @@ const getCompleteReport = async (req, res) => {
         // ])
         // console.log(desposition)
 
-       
-        // const desposition = await slpId.map( async (x)=>{ await Desposition.aggregate([
-        //     { $match: { userId: x } },
-        //     { $group: { _id: "$desposition", count: { $sum: 1 } } }
 
-        // ])})
+        // const desposition =  await Desposition.aggregate([
+        //     { $match: { userId: parentId } },
+        //     { $group: { _id: "$_id",desposition:{$first:"$desposition"}, count: { $sum: 1 } } }
+
+        // ])
         // console.log(desposition)
         const despositionReport = []
-        for (i=0;i<slpId.length;i++){
+        for (i = 0; i < slpId.length; i++) {
             slpCurrentId = slpId[i];
             const detail = await Desposition.aggregate([
-                    { $match: { userId: slpCurrentId } },
-                    { $group: { _id: "$desposition", count: { $sum: 1 } } }
-        
-                ])
-            despositionReport.push(detail)
+                { $match: { userId: slpCurrentId } },
+                { $group: { _id: "$desposition", count: { $sum: 1 } } }
+                // {$project : {_id:0,_id:0,desposition:1, count:1}}
+            ])
+            const singleDetail = Report_List2[detail[i]._id]
+            const singleDetailCount = detail[i].count
+            console.log(singleDetail, singleDetailCount)
+            despositionReport.push({
+                despName: singleDetail,
+                count: singleDetailCount
+            })
+            // despositionReport.push(detail)
         }
 
 
